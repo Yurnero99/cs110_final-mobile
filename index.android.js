66c4a8f4b6973ffcdd8b825e72927815028349ee
Robert Gevorgyan <robert1999.g@gmail.com>
2016-12-01 21:12:08 +0400

    Little Playing

--- index.android.js
+++ index.android.js
@@ -1,33 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
 } from 'react-native';
+import Homepage from './lib/Homepage';
 
-export default
 class Test extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Gesture!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Homepage styles={styles} />
     );
   }
 }
@@ -49,6 +30,11 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  buttons: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
 });
 
 AppRegistry.registerComponent('Test', () => Test);
